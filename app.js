d92b57390785aaf4325bea0c2e520c17080761c3
Rpriest <bamgbosesamueltofunmi@gmail.com>
2021-09-29 10:40:23 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req,res){
+    var fName = req.body.fName;
+    var lName = req.body.lName;
+    var email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: fName,
+                    LNAME: lName,                    
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://us5.api.mailchimp.com/3.0/lists/42d84dc117";
+    const options = {
+        method: "POST",
+        auth: "priest:05e870ccfa65d02bc2c7d9bdb109b322-us5"
+    }
+    const request = https.request(url, options, function(response){
+            if (response.statusCode === 200){
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+            response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running on port 3000");
+})
+
